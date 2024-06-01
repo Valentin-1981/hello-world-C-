21cddfb36864ce1930421baceb58d852f88597fb
Valentin-1981 <chibinyov@rambler.ru>
2024-06-01 08:37:17 +0300

    first commit

---
+++ Hello world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
